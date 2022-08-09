7d437003cf3ecabe29f3657a3614bf25749e40cd
Abid Wali <abidwali14@gmail.com>
2022-08-09 17:14:59 +0530

    Done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Demo App"),
+        title: const Text("Demo App"),
       ),
-      drawer: Drawer(),
-      body: Center(
-        child: Container(
-          child: Text("Hello and Welcome to Flutter"),
-        ),
+      drawer: const Drawer(),
+      body: const Center(
+        child: Text("Hello and Welcome to Flutter"),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:demo_app/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HomePage(),
     );
   }
